b8cc8b9aa95246f59db1ee4a5a4db0205c553740
jeremy-hornback-eis <142057040+jeremy-hornback-eis@users.noreply.github.com>
2026-09-13 01:39:22 +0000

    ## Patch notes for 2026.162.255
    These patch notes summarize the changes from version 154.0.181.
    
    Added 7 new endpoints:
    * Project.CreateProjectExport
    * Resource.BulkUpdateResources
    * ResourceTeam.GetResourceTeam
    * Risk.CreateRisk
    * Task.MarkTaskAsReadForTheCurrentUser
    * TaskStatus.MoveTasksToATaskStatus
    * WorkSpace.UpdateCurrentWorkspaceSettings
    
    Renamed 10 endpoints:
    * Renamed Meetings.CreateWeeklyRecurringMeetings to MeetingRecurrency.CreateWeeklyRecurringMeetings
    * Renamed Meetings.CreateMonthlyRecurringMeetings to MeetingRecurrency.CreateMonthlyRecurringMeetings
    * Renamed Meetings.CreateDailyRecurringMeetings to MeetingRecurrency.CreateDailyRecurringMeetings
    * Renamed Meetings.CreateYearlyRecurringMeetings to MeetingRecurrency.CreateYearlyRecurringMeetings
    * Renamed Meetings.DeleteRecurringMeetings to MeetingRecurrency.DeleteRecurringMeetings
    * Renamed Meetings.ValidateRecurringMeetingSettings to MeetingRecurrency.ValidateRecurringMeetingSettings
    * Renamed Meetings.UpdateRecurringMeetingSettings to MeetingRecurrency.UpdateRecurringMeetingSettings
    * Renamed ProjectMembers.UpdateUserProjectMembership to ProjectMembers.CreateOrUpdateUserProjectMembership
    * Renamed ResourceTeam.RetrieveResourceTeams to ResourceTeam.QueryResourceTeams
    * Renamed WorkSpace.RetrieveWorkspaces to WorkSpace.RetrieveCurrentWorkspace
    
    Changes to data models:
    * IntegrationProviderDto: Added new field `unauthorized`
    * MeetingUpdateDto: Added new field `breakRecurrency`
    * MeetingUpdateDto: Added new field `moveToProject`
    * NptUpdateDto: Added new field `breakRecurrency`
    * NptUpdateDto: Added new field `moveToProject`
    * RecurringTaskChangeSetDetails: Added new field `recurrences`
    * ResourceCreateDto: Added new field `defaultPlannedHours`
    * ResourceCreateDto: Added new field `workingDays`
    * ResourceDto: Added new field `roleId`
    * ResourceDto: Added new field `defaultPlannedHours`
    * ResourceDto: Added new field `workingDays`
    * ResourceUpdateDto: Added new field `defaultPlannedHours`
    * ResourceUpdateDto: Added new field `workingDays`
    * RiskUpdateDto: Added new field `moveToProject`
    * TaskDetailsDto: Added new field `baseline`
    * TaskFileDto: Added new field `size`
    * TaskOwnerDto: Added new field `name`
    * TaskOwnerDto: Added new field `shortName`
    * TaskUpdateDto: Added new field `breakRecurrency`
    * TaskUpdateDto: Added new field `moveToProject`
    * UserRolePermissionsDto: Added new field `useMcp`
    * WorkSpaceDto: Added new field `accountOwnerId`
    * WorkSpaceDto: Added new field `status`
    * WorkSpaceDto: Added new field `isActive`
    * WorkSpaceDto: Added new field `isReseller`
    * WorkSpaceDto: Added new field `licenses`
    * WorkSpaceDto: Added new field `settings`
    * WorkSpacePermissionsDto: Added new field `changeOwnEmail`
    * WorkSpacePermissionsDto: Added new field `useMcp`
    * WorkSpaceUserInfoDto: Added new field `workSpacePricingType`
    * WorkSpaceUserInfoDto: Added new field `workSpaceHasScheduledChanges`
    * WorkSpaceUserInfoDto: Added new field `workspaceCurrencySymbol`
    * WorkSpaceUserInfoDto: Added new field `workspaceCurrency`
    * WorkSpaceUserInfoDto: Added new field `entitlements`
    * WorkSpaceUserInfoDto: Added new field `isSocial`
    
    Deprecated 5 old endpoints:
    * Holiday.QueryResourceHolidays
    * Holiday.QueryCountryHolidays
    * Holiday.QueryGlobalHolidays
    * ProjectMembers.UpdateAListOfUserProjectMembership
    * ProjectMembers.CreateUserProjectMembership

--- src/Models/WorkSpaceUserInfoDto.cs
+++ src/Models/WorkSpaceUserInfoDto.cs
@@ -89,6 +89,16 @@ namespace ProjectManager.SDK.Models
         public string WorkSpaceStatus { get; set; }
 
         /// <summary>
+        /// This is pricing type of workspace
+        /// </summary>
+        public int? WorkSpacePricingType { get; set; }
+
+        /// <summary>
+        /// This is true if workspace has scheduled changes
+        /// </summary>
+        public bool? WorkSpaceHasScheduledChanges { get; set; }
+
+        /// <summary>
         /// This is true if the WorkSpace is in the Active or Trial state.
         /// </summary>
         public bool? WorkSpaceIsActive { get; set; }
@@ -109,9 +119,14 @@ namespace ProjectManager.SDK.Models
         public string WorkspaceCulture { get; set; }
 
         /// <summary>
-        /// The culture code used for currency formatting in the workspace.
+        /// The currency symbol used for currency formatting in the workspace (e.g., &quot;$&quot; for USD, &quot;€&quot; for EUR).
         /// </summary>
-        public string WorkspaceCurrencyCulture { get; set; }
+        public string WorkspaceCurrencySymbol { get; set; }
+
+        /// <summary>
+        /// The name of the currency used for currency formatting in the workspace (e.g., &quot;USD&quot; for US Dollar, &quot;EUR&quot; for Euro).
+        /// </summary>
+        public string WorkspaceCurrency { get; set; }
 
         /// <summary>
         /// The culture code (e.g., &quot;en-US&quot;) used for formatting and localization for the user.
@@ -119,13 +134,19 @@ namespace ProjectManager.SDK.Models
         public string UserCulture { get; set; }
 
         /// <summary>
-        /// The culture code used for currency formatting for the user.
-        /// </summary>
-        public string UserCurrencyCulture { get; set; }
-
-        /// <summary>
         /// The location of the user, which is a combination of city, state (US Only), and country.
         /// </summary>
         public string Location { get; set; }
+
+        /// <summary>
+        /// The entitlements active for this Workspace. Each entry is either a boolean switch or a
+        /// numeric limit.
+        /// </summary>
+        public EntitlementDto[] Entitlements { get; set; }
+
+        /// <summary>
+        /// True if the user signed in via a social provider (Google, Apple, or Microsoft).
+        /// </summary>
+        public bool? IsSocial { get; set; }
     }
 }
